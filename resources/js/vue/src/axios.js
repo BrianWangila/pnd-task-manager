eb13af60a4ac55d0c5e4caf971a8cbe9c7102b7c
BrianWangila <bwanjalah@gmail.com>
2023-04-03 18:37:13 +0300

    build app

--- resources/js/vue/src/axios.js
+++ resources/js/vue/src/axios.js
@@ -7,6 +7,7 @@ const axiosClient = axios.create({
     // baseURL: 'http://192.168.1.91:8000/api'
     // baseURL: 'http://127.0.0.1:8000/api'
     baseURL: 'https://taskmgmt-development.up.railway.app/api'
+
 })
 
 axiosClient.interceptors.request.use(config => {
