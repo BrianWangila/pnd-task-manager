0b38fd7a7f241ca328f3f4a25d287d9c071d7db2
BrianWangila <bwanjalah@gmail.com>
2023-04-06 16:57:10 +0300

    build for production

--- resources/js/vue/src/axios.js
+++ resources/js/vue/src/axios.js
@@ -4,9 +4,9 @@ import { useAuthStore } from "./stores/authStore";
 
 
 const axiosClient = axios.create({
-    baseURL: 'http://192.168.1.95:8000/api'
+    // baseURL: 'http://192.168.1.95:8000/api'
     // baseURL: 'http://127.0.0.1:8000/api'
-    // baseURL: 'https://taskmgmt-development.up.railway.app/api'
+    baseURL: 'https://taskmgmt-development.up.railway.app/api'
 
 })
 
