06863d1adc0d137059b3add8495142a8a6b0b7aa
peakaanddale <malindi@peakaanddale.com>
2023-04-14 11:41:33 +0300

    changed axios url to point to railway

--- resources/js/vue/src/axios.js
+++ resources/js/vue/src/axios.js
@@ -6,7 +6,8 @@ import { useAuthStore } from "./stores/authStore";
 const axiosClient = axios.create({
     // baseURL: 'http://192.168.1.95:8000/api'
     // baseURL: 'https://tms.peakanddale.co.ke/api'
-    baseURL: 'http://127.0.0.1:8000/api'
+    // baseURL: 'http://127.0.0.1:8000/api'
+    baseURL: 'https://taskmgmt-sandbox.up.railway.app/api'
     // baseURL: 'https://taskmgmt-development.up.railway.app/api'
 
 });
